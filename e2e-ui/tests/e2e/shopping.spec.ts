12c8e24d0548370d300361dd73fa78026c65dc44
Camilo Soto <occamilo97@gmail.com>
2026-01-19 23:47:14 -0500

    Merge e2e-ui into main repository

---
+++ e2e-ui/tests/e2e/shopping.spec.ts
@@ -0,0 +1,9 @@
+import test from "@playwright/test";
+
+test.describe('Validación de funciones de compra en demoblaze', () => {
+    test('Agregar dos prodcutos al carrito', async ({ page }) => {
+        console.log('hola ');
+        
+    })
+    
+})
