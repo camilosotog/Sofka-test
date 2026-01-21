75beb1d38ebb7f250ed745e55f5e36fb7edb74ad
Camilo Soto <occamilo97@gmail.com>
2026-01-20 20:54:43 -0500

    Type: feat desc: add fill bill form and complete purchase steps

--- e2e-ui/tests/e2e/shopping.spec.ts
+++ e2e-ui/tests/e2e/shopping.spec.ts
@@ -1,10 +1,8 @@
 import test, { expect } from "@playwright/test";
-import { before } from "node:test";
-import { LoginPage } from "../pages/login.page";
-import { testUser } from "../data/user.data";
 import { BuyPage } from "../pages/buy.page";
 import { galaxyS7, sonyVaioI5 } from "../data/product.data";
 import { cartModule, homeModule } from "../data/module.data";
+import { billData } from "../data/bill.data";
 
 test.describe('Validación de funciones de compra en demoblaze', () => {
     test.beforeEach(async ({ page }) => {
@@ -17,23 +15,38 @@ test.describe('Validación de funciones de compra en demoblaze', () => {
         await test.step('Agregar dos prodcutos al carrito', async () => {
             const [responseProduct1] = await Promise.all([
                 page.waitForResponse(r => r.url().includes('addtocart') && r.status() === 200),
-                buyPage.addToCart(galaxyS7.name)
+                buyPage.addToCart(galaxyS7)
             ]);
             await expect(responseProduct1.status(), `El producto ${galaxyS7.name} NO se agrego al carrito`).toBe(200);
             await buyPage.goToModule(homeModule.name);
 
             const [responseProduct2] = await Promise.all([
                 page.waitForResponse(r => r.url().includes('addtocart') && r.status() === 200),
-                buyPage.addToCart(sonyVaioI5.name)
+                buyPage.addToCart(sonyVaioI5)
             ]);
             await expect(responseProduct2.status(), `El producto ${sonyVaioI5.name} -NO se agrego al carrito`).toBe(200);
         })
+
         await test.step('Visualizar el carrito', async () => {
             await buyPage.goToModule(cartModule.name);
             await expect(page.getByRole('heading', { name: 'Products' }), 'NO es la sección de carrito').toBeVisible();
             await expect(page.getByRole('cell', { name: galaxyS7.name }), 'Samsung galaxy s7 NO está visible').toBeVisible();
             await expect(page.getByRole('cell', { name: sonyVaioI5.name }), 'Sony vaio i5 NO está visible').toBeVisible();
         })
-    })
 
+        await test.step('Completar el formulario', async () => {
+            await buyPage.fillPurchaseForm(billData);
+            await expect(page.getByRole('textbox', { name: 'Name:' }), 'No se completó el campo Name').toHaveValue(billData.name);
+            await expect(page.getByRole('textbox', { name: 'Country:' }), 'No se completó el campo Country').toHaveValue(billData.country);
+            await expect(page.getByRole('textbox', { name: 'City:' }), 'No se completó el campo City').toHaveValue(billData.city);
+            await expect(page.getByRole('textbox', { name: 'Credit card:' }), 'No se completó el campo Credit card').toHaveValue(billData.creditCard);
+            await expect(page.getByRole('textbox', { name: 'Month:' }), 'No se completó el campo Month').toHaveValue(billData.month);
+            await expect(page.getByRole('textbox', { name: 'Year:' }), 'No se completó el campo Year').toHaveValue(billData.year);
+        })
+        
+        await test.step('Finalizar la compra', async () => {
+            await page.getByRole('button', { name: 'Purchase' }).click();
+            await expect(page.getByRole('heading', { name: 'Thank you for your purchase!' }), 'No se realizó la compra').toBeVisible(); 
+        })
+    })
 })
--- e2e-ui/tests/pages/buy.page.ts
+++ e2e-ui/tests/pages/buy.page.ts
@@ -1,3 +1,6 @@
+import { Bill } from "../interfaces/bill.interface";
+import { Product } from "../interfaces/product.interface";
+
 export class BuyPage {
     private page;
 
@@ -5,12 +8,22 @@ export class BuyPage {
         this.page = page;
     }
 
-    async addToCart(productName: string) {
-        await this.page.getByRole('link', { name: productName }).click();
+    async addToCart(product: Product) {
+        await this.page.getByRole('link', { name: product.name }).click();
         await this.page.getByRole('link', { name: 'Add to cart' }).click();
     }
 
     async goToModule(moduleName) {
         await this.page.getByRole('link', { name: moduleName, exact: true }).click();
+    }
+
+    async fillPurchaseForm(bill: Bill) {
+        await this.page.getByRole('button', { name: 'Place Order' }).click();
+        await this.page.locator('#name').fill(bill.name);
+        await this.page.locator('#country').fill(bill.country);
+        await this.page.locator('#city').fill(bill.city);
+        await this.page.locator('#card').fill(bill.creditCard);
+        await this.page.locator('#month').fill(bill.month);
+        await this.page.locator('#year').fill(bill.year);
     }
 }
